8ebd79dc2055860eb3ee24bdba3a5fecbe627aba
EC2 Default User <ec2-user@ip-10-0-1-57.ap-northeast-2.compute.internal>
2026-06-25 22:10:46 +0900

    update bedrock_api

--- bedrock_work/completed/api/bedrock_api.py
+++ bedrock_work/completed/api/bedrock_api.py
@@ -3,37 +3,46 @@ import boto3
 
 session = boto3.Session()
 
-bedrock = session.client(service_name='bedrock-runtime') #creates a Bedrock client
-
-bedrock_model_id = "us.amazon.nova-2-lite-v1:0" #set the foundation model
-
+bedrock = session.client(service_name='bedrock-runtime', region_name='us-east-1') #creates a Bedrock client
+bedrock_model_id = "global.anthropic.claude-sonnet-4-5-20250929-v1:0" #set the foundation model
 prompt = "What is the largest city in New Hampshire?" #the prompt to send to the model
 
 messages = [
     {
         "role": "user",
         "content": [
-            {"text": prompt}
+            {
+                "type": "text",                
+                "text": prompt
+            }
         ]
     }
 ]
 
 body = json.dumps({
-    "schemaVersion": "messages-v1",
+    "anthropic_version": "bedrock-2023-05-31",
     "messages": messages,
-    "inferenceConfig": {
-        "maxTokens": 1024,
-        "topP": 0.5,
-        "topK": 20,
-        "temperature": 0.0
-    }
+    "max_tokens": 1024,
+    "top_p": 0.5
+    
 }) #build the request payload
+# body = json.dumps({
+#     "schemaVersion": "messages-v1",
+#     "messages": messages,
+#     "inferenceConfig": {
+#         "max_tokens": 1024,
+#         "top_p": 0.5,
+#         "top_k": 20,
+#         "temperature": 0.0
+#     }
+# }) #build the request payload
 
 
 response = bedrock.invoke_model(body=body, modelId=bedrock_model_id, accept='application/json', contentType='application/json') #send the payload to Amazon Bedrock
 
-response_body = json.loads(response.get('body').read()) # read the response
+response_body = json.loads(response.get('body').read().decode('utf-8')) # read the response
 
-response_text = response_body["output"]["message"]["content"][0]["text"] #extract the text from the JSON response
+# response_text = response_body["output"]["message"]["content"][0]["text"] #extract the text from the JSON response
+response_text = response_body['content'][0]['text']
 
 print(response_text)
